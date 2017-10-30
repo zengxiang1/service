ef3f7513879c5e2c323f4f0d1d6b3c5427db08bb
xiangzeng <190093037@qq.com>
2017-10-30 18:04:20 +0800

    异常机制完善

---
+++ src/main/java/org/zx/learn/service/imp/BaseService.java
@@ -0,0 +1,16 @@
+package org.zx.learn.service.imp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.zx.learn.exception.ServiceException;
+
+/**
+ * Created by xiang zeng on 2017/10/30.
+ *
+ * @author xiang zeng
+ */
+public class BaseService {
+
+    private static final Logger log = LoggerFactory.getLogger(BaseService.class);
+
+}
